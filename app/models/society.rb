a8ff9d5fa442fc624d4c04a800a761ff8122c124
Giovanni Bisconti <bisconti@hotmail.it>
2010-11-29 16:27:42 +0100

    ho messo un pò di validazioni nei modelli

--- app/models/society.rb
+++ app/models/society.rb
@@ -1,5 +1,5 @@
 class Society < ActiveRecord::Base
-	validates_uniqueness_of :society, :message => "E' già stata inserita questa società'"
-	validates_uniqueness_of :email, :message => "L'email inserita è già utilizzata"
-	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => I18n.t ('email_format_error')
+	validates_uniqueness_of :society
+	validates_uniqueness_of :email
+	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => I18n.t('email_format_error')
 end
